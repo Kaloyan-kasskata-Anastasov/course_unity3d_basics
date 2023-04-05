4f97dd583a841b864ca1d36094903daa1aeaf8ba
kasskata <kassskata@gmail.com>
2023-04-05 13:46:59 +0300

    Demo 09. Advanced Scripting & Optimization - Coroutines

--- Assets/Scripts/DriverAI.cs
+++ Assets/Scripts/DriverAI.cs
@@ -1,16 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class DriverAI : MonoBehaviour
 {
     private Rigidbody body;
+    private Coroutine moveCoroutine;
+    public float speed = 10;
+    public bool IsAlive { get; set; } = true;
 
     private void Start()
     {
         body = GetComponent<Rigidbody>();
+        moveCoroutine = StartCoroutine(MoveCoroutine());
     }
 
-    private void Update()
+    public void OnDestroy()
     {
-        body.AddRelativeForce(new Vector3(0, 0, 10), ForceMode.Acceleration);
+        StopCoroutine(moveCoroutine);
+        moveCoroutine = null;
+    }
+
+    private IEnumerator MoveCoroutine()
+    {
+        while (IsAlive)
+        {
+            body.AddRelativeForce(new Vector3(0, 0, speed), ForceMode.Acceleration);
+            yield return new WaitForFixedUpdate();
+        }
     }
 }
