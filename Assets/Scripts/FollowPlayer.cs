1f36db58beca7ab66aed1c5729e74dc790ff7d43
kasskata <kassskata@gmail.com>
2023-04-06 20:49:37 +0300

    04. Using Game Object

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
